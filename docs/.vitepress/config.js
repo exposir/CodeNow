4ae5fd9ba151f8f3ed2158406017a7d578966c8d
孟世博 <897205285@qq.com>
2023-10-04 12:06:33 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -21,8 +21,11 @@ export default {
         },
 
         footer: {
-            message: 'Released under the MIT License.',
-            copyright: 'Copyright © 2019-present Evan You'
+            message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+            copyright: 'Copyright © 2019-present <a href="https://github.com/yyx990803">Evan You</a>'
+        },
+        search: {
+            provider: 'local'
         }
 
     },
