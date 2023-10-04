32c2608523b3376767064d091298c8ef25294a86
孟世博 <897205285@qq.com>
2023-10-04 11:10:46 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+// .vitepress/config.js
+export default {
+    // site-level options
+    title: '即刻编程',
+    description: 'Just coding now.',
+
+    themeConfig: {
+        // theme-level options
+    },
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+}
+
+
